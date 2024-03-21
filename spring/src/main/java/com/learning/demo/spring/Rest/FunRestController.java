335a4d9125ea9dd472b040cdaeb59b06cb0d5bac
GS-Charan <gs.charan1605@gmail.com>
2024-03-21 23:18:21 +0530

    day two practice

--- spring/src/main/java/com/learning/demo/spring/Rest/FunRestController.java
+++ spring/src/main/java/com/learning/demo/spring/Rest/FunRestController.java
@@ -1,5 +1,18 @@
 package com.learning.demo.spring.Rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
+	@Value("${my.name}")
+	String name;
+	@GetMapping("/")
+	public String hello()
+	{
+		
+		return name;
+	}
 
 }
