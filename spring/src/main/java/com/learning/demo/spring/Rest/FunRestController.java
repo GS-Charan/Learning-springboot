22032a6250e25baf6403f024687603bbfe3051c6
GS-Charan <gs.charan1605@gmail.com>
2024-04-14 21:26:53 +0530

    receive data and send back response

--- spring/src/main/java/com/learning/demo/spring/Rest/FunRestController.java
+++ spring/src/main/java/com/learning/demo/spring/Rest/FunRestController.java
@@ -1,18 +1,19 @@
 package com.learning.demo.spring.Rest;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-	@Value("${my.name}")
-	String name;
-	@GetMapping("/")
-	public String hello()
-	{
-		
-		return name;
-	}
-
+	
+	@PostMapping("/api/data")
+	   public ResponseEntity<String> receiveData(@RequestBody String data) {
+        // Process the received data
+        System.out.println("Received data: " + data);
+        return new ResponseEntity<>("recived data from springboot "+data, HttpStatus.OK);
+    }
+	//@RequestParam("data") String data
 }
---
+++ spring/src/main/java/com/learning/demo/spring/Rest/mydata.java
@@ -0,0 +1,6 @@
+package com.learning.demo.spring.Rest;
+
+public class mydata {
+	
+
+}
