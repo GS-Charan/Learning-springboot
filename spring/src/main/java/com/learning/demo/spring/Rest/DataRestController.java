a407e07b517bc3d0861e0c8bca76371d790d98c6
GS-Charan <gs.charan1605@gmail.com>
2024-04-17 23:42:06 +0530

    Created api endpoint to fetch all names from database

---
+++ spring/src/main/java/com/learning/demo/spring/Rest/DataRestController.java
@@ -0,0 +1,39 @@
+package com.learning.demo.spring.Rest;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.demo.spring.repository.DataRepository;
+import com.learning.demo.spring.model.Data;
+@RestController
+@RequestMapping("/api")
+public class DataRestController {
+	@Autowired
+	DataRepository datarepository;
+	@GetMapping("/names/fetchAll")
+	  public ResponseEntity<List<Data>> getAllDatas(@RequestParam(required = false) String name) {
+	    try {
+	      List<Data> names = new ArrayList<Data>();
+	      datarepository.findAll().forEach(names::add);
+	      
+	      if (names.isEmpty()) {
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	      }
+
+	      return new ResponseEntity<>(names, HttpStatus.OK);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+
+}
