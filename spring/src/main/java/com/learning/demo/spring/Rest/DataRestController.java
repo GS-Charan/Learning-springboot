968e85d642119061e63bcdfa723bcf565f3e0181
GS-Charan <gs.charan1605@gmail.com>
2024-04-18 23:07:35 +0530

    added api endpoint to save data to database

--- spring/src/main/java/com/learning/demo/spring/Rest/DataRestController.java
+++ spring/src/main/java/com/learning/demo/spring/Rest/DataRestController.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class DataRestController {
 	@Autowired
 	DataRepository datarepository;
 	@GetMapping("/names/fetchAll")
-	  public ResponseEntity<List<Data>> getAllDatas(@RequestParam(required = false) String name) {
+	  public ResponseEntity<List<Data>> getAllData(@RequestParam(required = false) String name) {
 	    try {
 	      List<Data> names = new ArrayList<Data>();
 	      datarepository.findAll().forEach(names::add);
@@ -35,5 +36,19 @@ public class DataRestController {
 	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 	  }
-
+	@PostMapping("/name/send")
+	public ResponseEntity<Data> addData(@RequestBody Data data)
+	{
+		try {
+	      Data data1=datarepository.save(data);
+	      
+		return new ResponseEntity<>(data1, HttpStatus.CREATED);
+		}
+		catch(Exception e)
+		{
+			return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
 }
